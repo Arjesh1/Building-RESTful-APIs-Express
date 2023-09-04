0acdc167e7ed47a2545160d6c9d1397b37916e06
Arjesh <arjesh.khadka2@gmail.com>
2023-09-05 06:56:41 +1000

    batabase setup

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/routes";
 
 const app = express();
 const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/ApiDb", {
+  useNewUrlParser: true,
+});
+
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 
 app.get("/", (req, res) => {
   res.send(`Node and express is running on port ${PORT}`);
